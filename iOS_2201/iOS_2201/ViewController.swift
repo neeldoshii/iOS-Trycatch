3ec1c30f8339a7044d329f0d0cbd798761a2597b
Neel Doshi <60827173+neeldoshii@users.noreply.github.com>
2022-01-22 17:39:57 +0530

    maybe :D

--- iOS_2201/iOS_2201/ViewController.swift
+++ iOS_2201/iOS_2201/ViewController.swift
@@ -13,13 +13,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let lbl = UILabel(frame: CGRect(x: 25, y: 200, width: 500, height: 200))
+        let lbl = UILabel(frame: CGRect(x: 25, y: 50, width: 500, height: 200))
         lbl.text = "Hello World"
+        lbl.textColor = UIColor.brown
+        lbl.textAlignment = NSTextAlignment.center
         self.view.addSubview(lbl)
     
-                          
+       let tf = UITextField(frame: CGRect(x: 10, y: 300, width: 500, height: 200))
+        
+        tf.placeholder = "Haha maybe placeholder is present :D"
+        self.view.addSubview(tf)
+        
+        let txtview = UITextView(frame: CGRect(x: 10, y: 550, width: 500, height: 100))
+        self.view.addSubview(txtview)
+        
+        
+        let btn = UIButton(frame: CGRect(x: 10, y: 650, width: 500, height: 100))
+        btn.setTitle("SignUp Maybe", for: .normal)
+        self.view.addSubview(btn)
+        btn.layer.cornerRadius = 5.0
+        btn.layer.borderWidth = 5.0
+        btn.layer.borderColor = UIColor.black.cgColor
+        
     }
-
-
 }
 
